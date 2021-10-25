fcd456261dba747d7bab9809f42c23d94a9f8e5b
Trevor L Davis <trevor.l.davis@gmail.com>
2021-10-25 11:28:32 -0700

    feat: Update to GNU Unifont v14.0.01
    closes #1

--- NEWS.md
+++ NEWS.md
@@ -1,10 +1,23 @@
-hexfont 0.1.2
+hexfont 0.2.0
+=============
+
+* We update the hex font files to match [GNU Unifont](http://unifoundry.com/unifont/index.html) v14.0.01 (previously matched v13.0.06).
+
+Breaking Changes
+----------------
+
+* Included hex font files are now compressed by `xz` instead `gzip`
+  and now end with a `.xz` instead of `.gz`.
+  This reduces file sizes by about a third.
+  Please note the function `unifont()` is still backwards compatible.
+
+hexfont 0.1.1
 =============
 
 * Initial release
 * Includes all the hex font files included in the [GNU Unifont](http://unifoundry.com/unifont/index.html) 
   source code distribution (v13.0.06).
   The version number is stripped from the file names in the `precompiled` directory and
-  the hex fonts are all compressed by `xz` but other than that the hex fonts are otherwise unchanged.
+  the hex fonts are all compressed by `gzip` but other than that the hex fonts are otherwise unchanged.
 * The function `unifont()` loads in several GNU Unifont hex files as a
   single `bittermelon::bm_font()` object.
---
+++ raw-data/update-unifont.R
@@ -0,0 +1,29 @@
+src_dir <- "raw-data/src/unifont-14.0.01"
+tar_dir <- "inst/font"
+
+for (doc in c("ChangeLog", "OFL-1.1.txt", "COPYING", "README"))
+    file.copy(file.path(src_dir, doc), file.path(tar_dir, doc), overwrite = TRUE)
+
+plane_files <- list.files(src_dir, pattern = "plane", recursive = TRUE)
+plane_files <- plane_files[-grep("plane02-jp.html", plane_files)]
+plane_files <- plane_files[-grep("bmp", plane_files)]
+
+for (f in plane_files) {
+    f_src <- file.path(src_dir, f)
+    f_tar <- file.path("inst", f)
+    file.copy(f_src, f_tar, overwrite = TRUE)
+    if (tools::file_ext(f) == "hex") {
+        system(paste("xz --force", f_tar))
+    }
+}
+
+precompiled_hex <- list.files(file.path(src_dir, "font/precompiled"), pattern = ".hex$",
+                              recursive = TRUE)
+
+for (f in precompiled_hex) {
+    f_src <- file.path(src_dir, "font/precompiled", f)
+    f_tar <- file.path(tar_dir, "precompiled", f)
+    f_tar <- gsub("(-[0-9.]*).hex", ".hex", f_tar)
+    file.copy(f_src, f_tar, overwrite = TRUE)
+    system(paste("xz --force", f_tar))
+}
